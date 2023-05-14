80c043341b8467b92364d15144e80cd283a454d8
revenz <revensleepz@gmail.com>
2023-05-13 18:51:48 -0600

    Added discord invite path

---
+++ app/routes/discord.tsx
@@ -0,0 +1,16 @@
+import { Component } from "react";
+
+class Discord extends Component {
+  componentDidMount() {
+    window.location.href = 'https://discord.gg/we-love-you-gabloxia-768962587462598697';
+  }
+
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+
+export default Discord;
